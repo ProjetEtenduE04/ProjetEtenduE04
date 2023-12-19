3f552fe85f314cfa84e36afac0290560bb97443f
jordan <6216948@cegepmontpetit.ca>
2023-12-19 02:04:51 -0500

    REFACTOR ListeAttente Model
    ajout de Data annotations au Model ListeAttente, puis un refactor de la propriete EstDisponible pour EstOuverte

--- Clinique_2000_E04/Clinique2000_Core/Models/ListeAttente.cs
+++ Clinique_2000_E04/Clinique2000_Core/Models/ListeAttente.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,15 +14,33 @@ namespace Clinique2000_Core.Models
         [Key]
         public int ListeAttenteID { get; set; }
 
-        public bool IsDisponible { get; set; }
+        public bool IsOuverte { get; set; }
 
+
+        [Display(Name = "Date d'effectivité")]
+        [Required(ErrorMessage ="Veuillez entrer une date d'effectivité pour la liste d'attente")]
+        [DataType(DataType.Date)]
         public DateTime DateEffectivite { get; set; }
 
+
+        [Display(Name = "Heure de fermeture")]
+        [Required(ErrorMessage = "Veuillez entrer l'heure d'ouverture de la liste d'attente")]
+        [DataType(DataType.Time)]
         public DateTime HeureOuverture { get; set; }
 
+
+        [Display(Name = "Heure d'ouverture")]
+        [Required(ErrorMessage = "Veuillez entrer l'heure de fermeture de la liste d'attente")]
+        [DataType(DataType.Time)]
         public DateTime HeureFermeture { get; set; }
 
+
+        [Display(Name = "Nombre de médecins disponibles")]
+        [Required(ErrorMessage="Veillez entrer le nombre de médecins disponibles")]
         public int NbMedecinsDispo { get; set; }
+
+
+
 
         ////proprietes de naviguation
 
