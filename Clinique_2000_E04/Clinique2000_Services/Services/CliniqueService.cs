8e84aaa802c7988d1c8ff199dec65a3e96a35ee4
Jordan Couture-Lafranchise <6216948@cegepmontpetit.ca>
2024-02-01 21:57:24 -0500

    FCT_AjoutMap
    modification de la methode Obtenir5CliniquesPlusProches
    modification action du controlleur
    modification vue

--- Clinique_2000_E04/Clinique2000_Services/Services/CliniqueService.cs
+++ Clinique_2000_E04/Clinique2000_Services/Services/CliniqueService.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -272,6 +273,7 @@ namespace Clinique2000_Services.Services
         {
             var patientID = await _consultationService.ObtenirIdPatientAsync();
             var patient = await _context.Patients.FirstOrDefaultAsync(x => x.PatientId == patientID);
+            var patientLocation = await _adresseService.GetLocationByPostalCodeAsync(patient.CodePostal);
 
             List<CliniqueDistanceVM> cliniquesAvecDistance = new List<CliniqueDistanceVM>();
 
@@ -284,8 +286,8 @@ namespace Clinique2000_Services.Services
 
                 if (aListeAttenteOuverteAvecConsultations)
                 {
-                    double distance = await _adresseService.CalculerDistanceEntre2CodesPostaux(
-                        clinique.Adresse.CodePostal, patient.CodePostal);
+                    var adresseClinique = await _adresseService.GetLocationByPostalCodeAsync(clinique.Adresse.CodePostal);
+                    double distance = await  _adresseService.CalculerDistanceEntre2CodesPostaux( patientLocation.PostalCode,adresseClinique.PostalCode);
 
                     var prochainePlageHoraire = _context.PlagesHoraires
                         .Include(ph => ph.Consultations)
@@ -298,7 +300,11 @@ namespace Clinique2000_Services.Services
                     {
                         Clinique = clinique,
                         Distance = distance,
-                        HeureProchaineConsultation = prochainePlageHoraire?.HeureDebut
+                        HeureProchaineConsultation = prochainePlageHoraire?.HeureDebut,
+                        CliniqueLatitude = adresseClinique.Latitude.ToString(CultureInfo.InvariantCulture),
+                        CliniqueLongitude = adresseClinique.Longitude.ToString(CultureInfo.InvariantCulture),
+                        PatientLatitude = patientLocation.Latitude.ToString(CultureInfo.InvariantCulture),
+                        PatientLongitude = patientLocation.Longitude.ToString(CultureInfo.InvariantCulture)
                     });
                 }
             }
@@ -309,6 +315,7 @@ namespace Clinique2000_Services.Services
                     .Take(5)
                     .ToList();
         }
+
 
 
     }
