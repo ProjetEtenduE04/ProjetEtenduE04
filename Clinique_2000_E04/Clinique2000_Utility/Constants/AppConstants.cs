8997099ce4c70010b6e3e222613b416c95dc2436
AnhNguyen <1904518@cegepmontpetit.ca>
2024-02-05 18:21:06 -0500

    FCT Modifier UI
    En-FN, padding, couleur...

--- Clinique_2000_E04/Clinique2000_Utility/Constants/AppConstants.cs
+++ Clinique_2000_E04/Clinique2000_Utility/Constants/AppConstants.cs
@@ -13,7 +13,7 @@ namespace Clinique2000_Utility.Constants
          @"F:\Projet\Clone\Clinique_2000\Clinique_2000_E04\Clinique2000_Utility\CodesPostauxQuebec\QuebecPostalCodes202312.csv";
         //@"C:\Users\misle\OneDrive\Bureau\final\Clinique_2000\Clinique_2000_E04\Clinique2000_Utility\CodesPostauxQuebec\QuebecPostalCodes202312.csv";
         //@"C:\Users\1495397\Desktop\Clinique_2000\Clinique_2000_E04\Clinique2000_Utility\CodesPostauxQuebec\QuebecPostalCodes202312.csv";
-        //@"C:\Ecole\Aut 2023\Projet final\Clinique_2000\Clinique_2000_E04\Clinique2000_Utility\CodesPostauxQuebec\QuebecPostalCodes202312.csv";
+        @"C:\Ecole\Aut 2023\Projet final\Clinique_2000\Clinique_2000_E04\Clinique2000_Utility\CodesPostauxQuebec\QuebecPostalCodes202312.csv";
         //@"C:\Users\6216948\Desktop\Clinique_2000\Clinique_2000_E04\Clinique2000_Utility\CodesPostauxQuebec\QuebecPostalCodes202312.csv";
 
 
