55b71da0db3faf8010ce38786e9a23640da024d5
Francois Gervais <0377892@cegepmontpetit.ca>
2024-03-06 16:16:15 -0500

    PROGRESS: Ajout VMs pour transiger l'information

---
+++ Clinique_2000_E04/Clinique2000_Core/ViewModels/ApprobationVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinique2000_Core.ViewModels
+{
+    /// <summary>
+    /// Contient la liste des objets à approuver
+    /// </summary>
+    public class ApprobationVM
+    {
+        // Défini quel type d'objets seront manipulés
+        public string Param { get; set; }
+
+        // Liste des cliniques à approuver
+        public ApprobationCliniquesVM ApprobationCliniquesVM { get; set; }
+
+        // Liste des utilisateurs à approuver
+        public ApprobationUtilisateursVM ApprobationUtilisateursVM { get; set; }
+    }
+}
