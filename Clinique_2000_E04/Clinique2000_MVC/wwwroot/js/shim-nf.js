e9dec50681e0c7ea6042e2814374431d157cd7c9
Francois Gervais <0377892@cegepmontpetit.ca>
2024-03-19 17:54:22 -0400

    PROGRESS: Ajustement JS
    TODO: Finaliser 4ème méthode

--- Clinique_2000_E04/Clinique2000_MVC/wwwroot/js/shim-nf.js
+++ Clinique_2000_E04/Clinique2000_MVC/wwwroot/js/shim-nf.js
@@ -3,15 +3,15 @@ document.addEventListener('keydown', function (event) {
         event.preventDefault();
         document.querySelector('form[action="/Cliniques/listeattente/AppelerProchainPatient"]').submit();
     }
-    else if (event.keyCode === 50) {
+    if (event.keyCode === 50) {
         event.preventDefault();
         document.querySelector('form[action="/Cliniques/listeattente/TerminerConsultationEtAppellerProchainPatient"]').submit();
     }
-    else if (event.keyCode === 51) {
+    if (event.keyCode === 51) {
         event.preventDefault();
         document.querySelector('form[action="/Cliniques/listeattente/TerminerConsultation"]').submit();
     }
-    else if (event.keyCode === 52) {
+    if (event.keyCode === 52) {
         event.preventDefault();
         document.querySelector('form[action="/Cliniques/listeattente/TerminerConsultationEtAppellerProchainPatient"]').submit();
     }
