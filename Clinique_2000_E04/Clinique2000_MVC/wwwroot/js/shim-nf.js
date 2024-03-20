bebe510d9568602aaa984595537d6c060b7316bb
Francois Gervais <0377892@cegepmontpetit.ca>
2024-03-20 14:25:22 -0400

    PROGRESS: Modification de script terminé

--- Clinique_2000_E04/Clinique2000_MVC/wwwroot/js/shim-nf.js
+++ Clinique_2000_E04/Clinique2000_MVC/wwwroot/js/shim-nf.js
@@ -1,18 +1,31 @@
 document.addEventListener('keydown', function (event) {
     if (event.keyCode === 49) {
         event.preventDefault();
-        document.querySelector('form[action="/Cliniques/listeattente/AppelerProchainPatient"]').submit();
+        var form = document.getElementById('form1');
+        form.action = '/Cliniques/ListeAttente/AppelerProchainPatient';
+        form.submit();
     }
+});
+document.addEventListener('keydown', function (event) {
     if (event.keyCode === 50) {
         event.preventDefault();
-        document.querySelector('form[action="/Cliniques/listeattente/TerminerConsultationEtAppellerProchainPatient"]').submit();
+        var form = document.getElementById('form2');
+        form.action = '/Cliniques/ListeAttente/TerminerConsultationEtAppellerProchainPatient';
+        form.submit();
     }
+});
+document.addEventListener('keydown', function (event) {
     if (event.keyCode === 51) {
         event.preventDefault();
-        document.querySelector('form[action="/Cliniques/listeattente/TerminerConsultation"]').submit();
+        var form = document.getElementById('form2');
+        form.action = '/Cliniques/ListeAttente/TerminerConsultation';
+        form.submit();
     }
+});
+document.addEventListener('keydown', function (event) {
     if (event.keyCode === 52) {
         event.preventDefault();
-        document.querySelector('form[action="/Cliniques/listeattente/TerminerConsultationEtAppellerProchainPatient"]').submit();
-    }
+        var form = document.getElementById('form2');
+        form.action = '/Cliniques/ListeAttente/TerminerConsultationEtAppellerProchainPatient';
+        form.submit();    }
 });
